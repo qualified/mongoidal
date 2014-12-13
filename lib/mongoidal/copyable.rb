704624d9b4b307e46d43a2d2b439f6d9f453c1f9
jake hoffner <jake.hoffner@gmail.com>
2014-12-12 16:42:32 -0800

    upgraded the ability to make deep copies

--- lib/mongoidal/copyable.rb
+++ lib/mongoidal/copyable.rb
@@ -12,9 +12,25 @@ module Mongoidal
     end
 
     def copy_to(target)
+      target.attributes = reset_ids(attributes.dup)
+    end
+
+    def copy
+      self.class.new(reset_ids(attributes.dup))
+    end
+
+    protected
+
+    def reset_ids(attributes)
       attributes.each do |key, value|
-        if key != '_id'
-          target[key] = value
+        if key == '_id' and value.is_a?(BSON::ObjectId)
+          attributes[key] = BSON::ObjectId.new
+        elsif value.is_a?(Hash)
+          attributes[key] = reset_ids(value.dup)
+        elsif value.is_a?(Array)
+          attributes[key] = value.map do |v|
+            v.is_a?(Hash) ? reset_ids(v.dup) : v
+          end
         end
       end
     end
--- spec/copyable_spec.rb
+++ spec/copyable_spec.rb
@@ -1,16 +1,27 @@
 require 'spec_helper'
 
+class CopyableChild
+  include Mongoid::Document
+  field :label
+
+  embedded_in :copyable_example
+end
+
 class CopyableExample
   include Mongoid::Document
   include Mongoidal::Copyable
 
   field :name, type: String
   field :address, type: String
+
+  embeds_many :copyable_childs
 end
 
 describe Mongoidal::Copyable do
   let(:a) { CopyableExample.new(name: 'name', address: 'address') }
   let(:b) { CopyableExample.new }
+  let(:a_child) { a.copyable_childs.build(label: 'a') }
+  let(:b_child) { b.copyable_childs.first }
 
   describe '#copy_changes_to' do
     before do
@@ -32,11 +43,16 @@ describe Mongoidal::Copyable do
   end
 
   describe '#copy_to' do
+    before { a_child.save }
     before { a.copy_to(b) }
 
     it 'should copy fields' do
+      b.save
+      b.reload
       expect(b.name).to eq a.name
       expect(b.address).to eq a.address
+      expect(b_child.label).to eq 'a'
+      expect(b_child.id).not_to eq a_child.id
     end
 
     it 'should not copy id' do
