bc9a1e390dab6cb3c187d87f067545478a90d0ad
jhoffner <jake.hoffner@gmail.com>
2015-04-06 18:06:50 -0700

    bulk savable now by default includes all embedded many relations if non are specified

--- lib/mongoidal/bulk_savable.rb
+++ lib/mongoidal/bulk_savable.rb
@@ -1,10 +1,16 @@
 module Mongoidal
   module BulkSavable
+
     # allows updating of an entire document structure without having to replace existing embedded
     # documents. a special 'deleted' attribute is used to determine if a model should be deleted.
     # deleted models are not actually destroyed, but instead returned within a flat array which
     # can later be destroyed.
+    # If a specific set of embed relations are not provided then all embeds_many relations will be used
     def bulk_assign(attributes, *embeds)
+      if embeds.blank?
+        embeds = self.class.relations.select {|k, v| v.macro == :embeds_many }.keys
+      end
+
       root_attrs = attributes.except(*embeds)
       embed_attrs = attributes.slice(*embeds)
 
@@ -16,7 +22,13 @@ module Mongoidal
           embed_attrs[embed].each do |attrs|
             collection = self.send(embed)
             existing = collection.where(id: attrs[:id] || attrs['id']).first
+
+            if !existing and attrs['slug']
+              existing = collection.where(slug: attrs['slug']).first
+            end
+
             deleted = attrs[:deleted] || attrs['deleted']
+
             if existing
               if deleted
                 to_delete << existing
